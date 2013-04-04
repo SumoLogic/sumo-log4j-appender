e435adf70dc611451713a931b0ae5fdf24d936fc
Yongxing Wang <yongxing@sumologic.com>
2013-04-03 18:04:04 -0700

    SUMO-1 cleanup the resource and add parameters to control connection timeout/sockettimeout

--- src/main/java/com/sumologic/log4j/SumoLogicAppender.java
+++ src/main/java/com/sumologic/log4j/SumoLogicAppender.java
@@ -31,7 +31,11 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Layout;
 import org.apache.log4j.helpers.LogLog;
@@ -47,22 +51,29 @@ import java.io.IOException;
 public class SumoLogicAppender extends AppenderSkeleton {
 
   private String url = null;
+  private int connectionTimeout = 1000;
+  private int socketTimeout = 60000;
 
   private HttpClient httpClient = null;
 
   public void setUrl(String url) {
     this.url = url;
+  }
 
-    if (this.url.contains("format")) {
-      LogLog.warn("URL contains the term 'format' -- this may cause issues if set to false.");
-    } else {
-      this.url += "&format=multiline";
-    }
+  public void setConnectionTimeout(int connectionTimeout) {
+    this.connectionTimeout = connectionTimeout;
+  }
+
+  public void setSocketTimeout(int socketTimeout) {
+    this.socketTimeout = socketTimeout;
   }
 
   @Override
   public void activateOptions() {
-    httpClient = new DefaultHttpClient(new ThreadSafeClientConnManager());
+    HttpParams params = new BasicHttpParams();
+    HttpConnectionParams.setConnectionTimeout(params, connectionTimeout);
+    HttpConnectionParams.setSoTimeout(params, socketTimeout);
+    httpClient = new DefaultHttpClient(new ThreadSafeClientConnManager(), params);
   }
 
   @Override
@@ -71,7 +82,7 @@ public class SumoLogicAppender extends AppenderSkeleton {
       return;
     }
 
-    StringBuilder builder = new StringBuilder();
+    StringBuilder builder = new StringBuilder(1024);
     builder.append(layout.format(event));
     if (layout.ignoresThrowable()) {
       String[] throwableStrRep = event.getThrowableStrRep();
@@ -109,16 +120,20 @@ public class SumoLogicAppender extends AppenderSkeleton {
   }
 
   private void sendToSumo(String log) {
+    HttpPost post = null;
     try {
-      HttpPost post = new HttpPost(url);
+      post = new HttpPost(url);
       post.setEntity(new StringEntity(log, HTTP.PLAIN_TEXT_TYPE, HTTP.UTF_8));
       HttpResponse response = httpClient.execute(post);
       int statusCode = response.getStatusLine().getStatusCode();
       if (statusCode != 200) {
         LogLog.warn(String.format("Received HTTP error from Sumo Service: %d", statusCode));
       }
+      //need to consume the body if you want to re-use the connection.
+      EntityUtils.consume(response.getEntity());
     } catch (IOException e) {
       LogLog.warn("Could not send log to Sumo Logic", e);
+      try { post.abort(); } catch (Exception ignore) {}
     }
   }
 }
